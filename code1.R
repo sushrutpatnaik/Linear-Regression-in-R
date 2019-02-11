f7ddf309f2af0ecc72a34dc99296739df073a5d2
SUSHRUT PATNAIK <patnaiksushrut@gmail.com>
2019-02-10 23:40:41 -0600

    Final Submission

--- code1.R
+++ code1.R
@@ -3,80 +3,103 @@
 housing_data<-read.csv("https://www.utdallas.edu/~sxp175331/datasets/housing.csv")
 
 
+#Since Ocean proximity is a categorical variable
+#we need to find all the disntinct categories
 
-#view the dataset and its structure
-View(housing_data)
-str(housing_data)
+#x <- unique(housing_data$ocean_proximity)
 
-detach(housing_data2)
+#encoding all categories to numbers
 
+#housing_data$ocean_proximity= factor(housing_data$ocean_proximity,
+#                                     levels = c('<1H OCEAN','INLAND','ISLAND','NEAR BAY','NEAR OCEAN'),
+#                                     labels = c(1,2,3,4,5))
 
-#scale the data
-housing_data[1:8] <- lapply(housing_data[1:8], function(x) c(scale(x)))
+#-----------------------convert categories
 
-#from the structure we can see that "ocean_proximity" has 5 categories
-View(housing_data)
+library(dplyr)
 
-#transform categorical data to make each category as a predictor varaible
-housing_data2<-cbind(housing_data[1:9], sapply(levels(housing_data$ocean_proximity), function(x) as.integer(x == housing_data$ocean_proximity)))
+categories = unique(housing_data$ocean_proximity)
+#split the categories off
+cat_housing = data.frame(ocean_proximity = housing_data$ocean_proximity)
 
-#view the updated dataset
-View(housing_data2)
-str(housing_data2)
-
-#now we have 13 predictor variables
-
-
-#ignore this block for now
-#convert categorical into numerical
-#convert<-sapply(housing_data,is.factor)       
-#ocean_proximity<-sapply(housing_data[,convert],unclass)    
-#housing_data<-cbind(housing_data[,!convert],ocean_proximity)  
-
-
-#check if the dataset contains NULL values
-print('checking if any null values')
-na_count <-sapply(housing_data, function(y) sum(is.na(y)))
-na_count <- data.frame(na_count)  
-na_count
-
-#We get 207 null values in total_bedrooms column
-
-#So we replace Null value in a column with its mean
-for(i in 1:ncol(housing_data2)){
-  housing_data2[is.na(housing_data2[,i]), i] <- mean(housing_data2[,i], na.rm = TRUE)
+for(cat in categories){
+  cat_housing[,cat] = rep(0, times= nrow(cat_housing))
 }
 
+for(i in 1:length(cat_housing$ocean_proximity)){
+  cat = as.character(cat_housing$ocean_proximity[i])
+  cat_housing[,cat][i] = 1
+}
+
+cat_columns = names(cat_housing)
+keep_columns = cat_columns[cat_columns != 'ocean_proximity']
+cat_housing = select(cat_housing,one_of(keep_columns))
+
+
+#-------------------------------------------------
+
+#check if there are any NULL values in any
+# of the columns
+
+print('checking if any null values')
+na_count <-sapply(housing_data, function(y) sum(is.na(y)))
+na_count <- data.frame(na_count)  # we got 207 null values in total_bedrooms column
+
+#replace Null value in a column with its mean
+
+for(i in 1:ncol(housing_data)){
+  housing_data[is.na(housing_data[,i]), i] <- mean(housing_data[,i], na.rm = TRUE)
+}
+
+#Scaling the numerical variables
+
+drops= c('ocean_proximity','median_house_value')
+housing_new_data=housing_data[, !(names(housing_data) %in% drops)]
+scaled_housing_new_data = scale(housing_new_data)
+
+housing_mod_data = cbind(cat_housing,scaled_housing_new_data,median_house_value=housing_data$median_house_value)
+
+
+par(mfrow=c(1,1))
 library(e1071)
 # density plot for 'median_house_value'
-plot(density(housing_data2$median_house_value), main="Density Plot: Median House Price ", 
-     ylab="Frequency", sub=paste("Skewness:", round(e1071::skewness(housing_data2$median_house_value), 2)))  
-polygon(density(housing_data2$median_house_value), col="red")
+plot(density(housing_mod_data$median_house_value), main="Density Plot: Median House Price ", 
+     ylab="Frequency", sub=paste("Skewness:", round(e1071::skewness(housing_mod_data$median_house_value), 2)))  
+polygon(density(housing_mod_data$median_house_value), col="red")
 
 #we see that the distribution for Median House Price is not normal
 
-attach(housing_data2)
+attach(housing_mod_data)
 
+par(mfrow=c(1,1))
 #check the correlation between various variables using corrplot
 require(corrplot)
-corrplot(cor(housing_data2), method = "number")
+corrplot(cor(housing_mod_data), method = "number")
 
 #we see that some of the features like 'total_rooms', 'total_bedrooms', 'population'and 'households' are highly correlated with each other.
 #Also latitude and longitude are highly correlated which is obvious as we are using california state data 
 
 #There is moderate linear relationship between median income and and house value
-plot(housing_data2$median_house_value ~ housing_data2$median_income, xlab = "Median Income", ylab = "Median House Price")
+par(mfrow=c(1,1))
+plot(housing_mod_data$median_house_value ~ housing_mod_data$median_income, xlab = "Median Income", ylab = "Median House Price", col = 'blue')
+
 fit<-lm(median_house_value ~ median_income)
 summary(fit)
-
 abline(fit, lwd = 4, col = 'red')
 
+#scatter plots for median house value wrt longitude and wrt latitude 
+par(mfrow=c(1,2))
+plot(median_house_value~longitude, col = 'red')
+plot(median_house_value~latitude, col = 'red')
+#we dont see any correlation between them
 
+#scatter plots for median house value wrt 'total_rooms', 'total_bedrooms', 'population'and 'households'
+#par(mfrow=c(2,2))
+#plot(median_house_value~total_rooms)
+#plot(median_house_value~total_bedrooms)
+#plot(median_house_value~population)
+#plot(median_house_value~households)
 
-
-myvars <- c("median_house_value","longitude","latitude")
-housing_data3 = housing_data2[myvars]
-plot(housing_data3)
 
 
 
@@ -91,30 +114,20 @@ summary(fit1)
 
 fit2 <- lm(median_house_value ~ latitude + longitude + housing_median_age)
 summary(fit2)
-#we can see p value is around 0.02 so we cannot reject the null hypothesis ,
+#we can see p value is around 0.0187 so we cannot reject the null hypothesis ,
 #therefore we can conclude that housing_median_age column doesnot contribute
 #towards house price
 
-#Now lets add few more variables to the model
-fit3 <- lm(median_house_value ~ latitude + longitude + total_rooms + total_bedrooms +
-               population + households + median_income )
-summary(fit3) #every variable is pretty much significant
 
+#for fit3 we have added the Ocean Proximity variables.
+fit3 <- lm(median_house_value  ~ latitude + longitude + total_rooms + total_bedrooms + population + households + median_income + `<1H OCEAN` + INLAND + ISLAND + `NEAR OCEAN` + `NEAR BAY`)
+summary(fit3)
+#we can see p value is around 0.0217, 0.7516, and NA. so we cannot reject the null hypothesis ,
+#therefore we can conclude that Ocean Proximity variables do not contribute
+#towards house price
 
-#for fit4 we have added the Ocean Proximity variables.
-fit4 <- lm(median_house_value  ~ latitude + longitude + total_rooms + total_bedrooms + population + households + median_income + `<1H OCEAN` + INLAND + ISLAND + `NEAR OCEAN` + `NEAR BAY`)
+fit4 <- lm(median_house_value  ~ latitude + longitude + total_rooms + total_bedrooms + population + households + median_income )
 summary(fit4)
-
-#from fit4 we can see that NEAR OCEAN is insignificant and NEAR BAY has NA as coeffcients.
-#that means NEAR BAY is linearly related to some other variable. So we can drop these two
-
-#Dropping NEAR BAY and NEAR OCEAN
-fit5 <- lm(median_house_value  ~ latitude + longitude + total_rooms + total_bedrooms + population + households + median_income + `<1H OCEAN` + INLAND + ISLAND)
-summary(fit5)
-
-#we will do a combination of variable (product and sum)
-fit6 <- lm(median_house_value  ~ (latitude * longitude) + (total_bedrooms * total_rooms * population) + median_income + `<1H OCEAN` + INLAND + ISLAND)
-summary(fit6)
 
 #------------)------------------------------------------------------------------------------
 #target variable is median_house_value
@@ -122,15 +135,15 @@ summary(fit6)
 
 install.packages(caTools)
 library(caTools)
-split = sample.split(housing_data2$median_house_value,SplitRatio = 0.8)
+split = sample.split(housing_mod_data$median_house_value,SplitRatio = 0.8)
 
-training_data = subset(housing_data2,split == TRUE)
-test_data = subset(housing_data2,split == FALSE)
+training_data = subset(housing_mod_data,split == TRUE)
+test_data = subset(housing_mod_data,split == FALSE)
 
 
 #Fitting multiple Linear regression to the Training Set
 
-regressor = lm(formula = median_house_value  ~ latitude + longitude + total_rooms + total_bedrooms + population + households + median_income + `<1H OCEAN` + INLAND + ISLAND,
+regressor = lm(formula = median_house_value  ~ latitude + longitude + total_rooms + total_bedrooms + population + households + median_income,
                data = training_data)
 
 #Prediction on test set
@@ -139,8 +152,6 @@ y_pred = predict(regressor,newdata = test_data)
 
 #Residual sum of squares (RSS) is the sum of the squared residuals
 rss <- c(crossprod(regressor$residuals))
-
-
 mse <- rss/length(regressor$residuals)
 rmse <- sqrt(mse)
 rmse
